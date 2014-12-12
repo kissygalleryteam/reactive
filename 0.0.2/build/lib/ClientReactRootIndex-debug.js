b660bf101568433671ff9f948a76d52a1c7a1372
弘树 <tiehang.lth@alibaba-inc.com>
2014-12-12 10:55:18 +0800

    sync daily version 0.0.2

---
+++ 0.0.2/build/lib/ClientReactRootIndex-debug.js
@@ -0,0 +1,26 @@
+define('kg/react/0.0.2/lib/ClientReactRootIndex',[],function(require, exports, module) {
+ /**
+ * Copyright 2013-2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule ClientReactRootIndex
+ * @typechecks
+ */
+
+"use strict";
+
+var nextReactRootIndex = 0;
+
+var ClientReactRootIndex = {
+  createReactRootIndex: function() {
+    return nextReactRootIndex++;
+  }
+};
+
+module.exports = ClientReactRootIndex;
+
+});
---
+++ 0.0.2/build/lib/performance-debug.js
@@ -0,0 +1,29 @@
+define('kg/react/0.0.2/lib/performance',["./ExecutionEnvironment"],function(require, exports, module) {
+ /**
+ * Copyright 2013-2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule performance
+ * @typechecks
+ */
+
+"use strict";
+
+var ExecutionEnvironment = require("./ExecutionEnvironment");
+
+var performance;
+
+if (ExecutionEnvironment.canUseDOM) {
+  performance =
+    window.performance ||
+    window.msPerformance ||
+    window.webkitPerformance;
+}
+
+module.exports = performance || {};
+
+});

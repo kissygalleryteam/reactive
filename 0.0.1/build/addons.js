551144f076f3b3cd5063c5d59a8ef39fcab871f2
弘树 <tiehang.lth@alibaba-inc.com>
2014-12-13 04:12:33 +0800

    sync daily branch 0.0.2

--- 0.0.1/build/addons.js
+++
@@ -1,4 +0,0 @@
-define('kg/react/1.0/addons',["./lib/ReactWithAddons"],function(require, exports, module) {
- module.exports = require('./lib/ReactWithAddons');
-
-});
--- 0.0.1/build/react.js
+++
@@ -1,4 +0,0 @@
-define('kg/react/1.0/react',["./lib/React"],function(require, exports, module) {
- module.exports = require('./lib/React');
-
-});
